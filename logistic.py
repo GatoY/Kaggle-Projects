5d0ce2ebc04773c2769db5502d13a84473451496
GatoY <derakhy@gmail.com>
2018-03-13 11:23:15 +1100

    first commit

---
+++ logistic.py
@@ -0,0 +1,62 @@
+import numpy as numpy
+import pandas as pd
+from sklearn import tree
+from sklearn import preprocessing
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+def load_data(file_name):
+    data = pd.read_csv(file_name)
+    print '```````````````'
+    #print data.columns
+    print '````````````````'
+    fix_miss_value(data)
+    convert_data(data)
+    return data
+
+def fix_miss_value(df):
+    df.Embarked[df.Embarked.isnull()] = df.Embarked.dropna().mode().values
+    df.Age[df.Age.isnull()]=df.Age.dropna().mean()
+    del df['Cabin']
+
+def convert_data(df):
+    df.Sex[df['Sex']=='male']=1
+    df.Sex[df['Sex']=='female']=0
+    df['fare_bin']=pd.qcut(df.Fare,4)
+    df['fare_id']=pd.factorize(df.fare_bin)[0]+1
+    df['embarked_id']=pd.factorize(df.Embarked)[0]+1
+
+def off_feature_extraction(df):
+    new_df = df.drop(['fare_bin', 'Fare', 'PassengerId', 'Name', 'Ticket', 'Fare', 'Embarked', 'fare_bin'], axis=1)
+    new_df.columns = [0,1,2,3,4,5,6,7]
+    label = new_df[0].values
+    feature = new_df.ix[:,1:].values
+    return label, feature
+
+def test_feature_extraction(df):
+    new_df = df.drop(['fare_bin', 'Fare', 'PassengerId', 'Name', 'Ticket', 'Fare', 'Embarked', 'fare_bin'], axis=1)
+    new_df.columns = [0,1,2,3,4,5,6]
+    label_data = pd.read_csv('gender_submission.csv')
+    #print label_data.values
+    label = label_data['Survived'].values
+    feature = new_df.ix[:].values
+    return label, feature
+
+def off_model_building(feature,label):
+    train_feature, test_feature, train_label, test_label = cross_validation.train_test_split(feature, label, test_size=0.3, random_state = 0)
+    clf = RandomForestClassifier(random_state=1,n_estimators=150,min_samples_split=4,min_samples_leaf=2)
+    clf=clf.fit(train_feature,train_label)
+    return clf, test_label,test_feature
+
+train_data = load_data('train.csv')
+test_data = load_data('test.csv')
+#print '````````````````````````````````'
+#print train_data.columns
+#print '``````````````````````````````````'
+Tlabel, Tfeature = off_feature_extraction(train_data)
+Telabel, Tefeature = test_feature_extraction(test_data)
+clf, test_label, test_feature = off_model_building(Tfeature,Tlabel)
+print clf.score(Tefeature,Telabel)
+print '2'
+print clf.score(test_feature,test_label)
+
+
---
+++ preprocess.py
@@ -0,0 +1,49 @@
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+import pandas as pd
+import re
+from sklearn import preprocessing
+sns.set()
+data = pd.read_csv('train.csv')
+data = data.drop(['PassengerId'],axis=1)
+min_max_scaler = preprocessing.MinMaxScaler()
+data['Pclass'] = pd.DataFrame(min_max_scaler.fit_transform(pd.DataFrame(data['Pclass'])))
+
+data.drop(['Ticket',axis=1])
+
+
+data['FamilyMembers'] = data['Parch']+data['SibSp']
+data['FamilyMembers'] = pd.DataFrame(min_max_scaler.fit_transform(pd.DataFrame(data['FamilyMembers'])))
+
+def get_title(name):
+    pattern = r' (\w+)\.'
+    result_search = re.search(pattern,name)
+    if result_search:
+	return result_search.group(1)
+    return ''
+
+for dataset in data:
+    dataset['Title'] = dataset['Name'].apply(get_title)
+
+
+for dataset in data:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt','Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+data = data.fillna({'Embarked':'S'})
+
+for dataset in data:
+    dataset['Sex'] = dataset['Sex'].map({'female':0, 'male':1}).astype(int)
+    title_mapping = {'Mr':1, 'Miss':2 , 'Mrs':3, 'Master': 4, 'Rare': 5}
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+    dataset['Embarked'] = dataset['Embarked'].map({'S':0, 'C':1, 'Q':2}).astype(int)
+    dataset.loc[dataset['Age']<=10, 'Age'] = 0
+    dataset.loc[(dataset['Age']>10) & (dataset['Age']<=18)] = 1
+    dataset.loc[(dataset['Age']>18) & (dataset['Age']<=60)] = 2
+    dataset.loc[(dataset['Age']>60)] = 3
+ 
+
